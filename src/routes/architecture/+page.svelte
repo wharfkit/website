054a1117180b491151be6d266a7d5874eea64234
Aaron Cox <aaron@greymass.com>
2022-11-15 20:09:29 -0500

    Added architecture content from Google Docs

---
+++ src/routes/architecture/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Architecture from "./architecture.md"
+</script>
+
+<div class="stack center">
+  <Architecture />
+</div>
+
+<style>
+  div > :global(h2) {
+    font-size: var(--fs-600);
+  }
+  div > :global(p) {
+    font-size: var(--fs-400);
+  }
+</style>
