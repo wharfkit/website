b07e66ca41061d548215edc1d0b398e1cf4ad983
Dean Sallinen <dean@greymass.com>
2022-11-16 12:32:25 -0800

    fix progess page for video and transcript links

--- src/routes/progress/updates/[slug]/+page.svelte
+++ src/routes/progress/updates/[slug]/+page.svelte
@@ -2,10 +2,11 @@
   import Article from "$lib/components/Article.svelte"
   /** @type {import('./$types').PageData} */
   export let data
+  let { videolink, transcriptlink, title } = data.meta
 </script>
 
 <div class="center stack">
-  <Article date={new Date(data.meta.date)} title={data.meta.title}>
+  <Article date={new Date(data.meta.date)} {videolink} {transcriptlink} {title}>
     {@html data.PostContent}
   </Article>
 </div>
