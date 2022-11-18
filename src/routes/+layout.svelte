e6988179f576fdbf44b3262d3e84d2a4af876bb2
Dean Sallinen <dean@greymass.com>
2022-11-18 10:39:57 -0800

    first pass at opengraph metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,24 @@
 
 <svelte:head>
   <title>Wharf Kit</title>
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@wharfkit" />
+  <meta name="twitter:title" content="Wharf Kit" />
+  <meta
+    name="twitter:description"
+    content="A Web Client SDK being developed by Greymass and funded by
+  blockchains within the Antelope Coalition." />
+  <meta name="twitter:image" content="https://wharfkit.com/images/wharf-logo.svg" />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Wharf Kit" />
+  <meta
+    property="og:description"
+    content="A Web Client SDK being developed by Greymass and funded by
+  blockchains within the Antelope Coalition." />
+  <meta property="og:url" content="https://wharfkit.com/" />
+  <meta property="og:image" content="https://wharfkit.com/images/wharf-logo.svg" />
 </svelte:head>
 
 <div class="wrapper | center stack">
