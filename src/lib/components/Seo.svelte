3d80179f79567e46e647b0438fd6a4c3baa83598
Dean Sallinen <dean@greymass.com>
2022-11-21 16:40:14 -0800

    improve seo and add meta tags for progress pages

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let pageUrl = "https://wharfkit.com"
+  export let pageDescription =
+    "Wharf provides intuitive and reliable developer tools that make it easy to build Antelope-based web apps."
+  export let pageTitle = ""
+  let baseTitle = "Wharf Kit"
+  $: fullTitle = pageTitle ? baseTitle.concat(" | ", pageTitle) : baseTitle
+</script>
+
+<svelte:head>
+  <title>{fullTitle}</title>
+  <meta name="description" content={pageDescription} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@wharfkit" />
+  <meta name="twitter:title" content={fullTitle} />
+  <meta name="twitter:description" content={pageDescription} />
+  <meta name="twitter:image" content="https://wharfkit.com/images/wharf-logo.svg" />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={fullTitle} />
+  <meta property="og:description" content={pageDescription} />
+  <meta property="og:url" content={pageUrl} />
+  <meta property="og:image" content="https://wharfkit.com/images/wharf-logo.svg" />
+</svelte:head>
--- src/routes/progress/updates/[slug]/+page.svelte
+++ src/routes/progress/updates/[slug]/+page.svelte
@@ -2,14 +2,10 @@
   import Article from "$lib/components/Article.svelte"
   /** @type {import('./$types').PageData} */
   export let data
-  let { videolink, transcriptlink, title } = data.meta
 </script>
 
 <div class="center stack">
-  <Article date={new Date(data.meta.date)} {videolink} {transcriptlink} {title}>
-    {@html data.PostContent}
+  <Article {...data.post}>
+    {@html data.post.content}
   </Article>
 </div>
-
-<style>
-</style>
