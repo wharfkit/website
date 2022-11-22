06593c15b0bed11a4617b9342ea22801df3ac47e
Dean Sallinen <dean@greymass.com>
2022-11-21 16:40:14 -0800

    improve seo and add meta tags for progress pages

--- src/routes/progress/updates/[slug]/+page.server.ts
+++ src/routes/progress/updates/[slug]/+page.server.ts
@@ -2,13 +2,17 @@ import { error } from "@sveltejs/kit"
 
 export const load = async ({ params }) => {
   try {
-    const post = await import(`../${params.slug}.md`)
+    const markdown = await import(`../${params.slug}.md`)
     return {
-      PostContent: post.default.render().html,
-      meta: {
-        ...post.metadata,
-        date: new Date(post.metadata.date).toLocaleDateString("en-US", { timeZone: "UTC" }),
+      post: {
+        ...markdown.metadata,
+        content: markdown.default.render().html,
+        date: new Date(markdown.metadata.date).toLocaleDateString("en-US", { timeZone: "UTC" }),
         slug: params.slug,
+      },
+      meta: {
+        title: markdown.metadata.title,
+        description: markdown.default.render().html
       },
     }
   } catch (err) {
