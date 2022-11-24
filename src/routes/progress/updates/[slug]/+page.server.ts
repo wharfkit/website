8f546556fc0aabf35d854a645cb4be43cf2bd09e
Dean Sallinen <dean@greymass.com>
2022-11-23 16:21:49 -0800

    fix for html in socialcard preview

--- src/routes/progress/updates/[slug]/+page.server.ts
+++ src/routes/progress/updates/[slug]/+page.server.ts
@@ -1,18 +1,29 @@
 import { error } from "@sveltejs/kit"
 
+
+let getThumbnail = (url: string) => {
+  let videoID = /^.*\/(.*)$/m.exec(url)[1]
+  return `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`
+}
+
+let stripTags = (text: string) => /^<.*>(.*)<\/.*>$/gm.exec(text)
+
+
 export const load = async ({ params }) => {
   try {
     const markdown = await import(`../${params.slug}.md`)
+    let content = markdown.default.render().html
     return {
       post: {
         ...markdown.metadata,
-        content: markdown.default.render().html,
+        content,
         date: new Date(markdown.metadata.date).toLocaleDateString("en-US", { timeZone: "UTC" }),
         slug: params.slug,
       },
       meta: {
         title: markdown.metadata.title,
-        description: markdown.default.render().html
+        description: stripTags(content),
+        seoImage: getThumbnail(markdown.metadata.videolink)
       },
     }
   } catch (err) {
