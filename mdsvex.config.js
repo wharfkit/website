4e66a2bec081b1c053b28d6ab62ec227b402160e
Aaron Cox <aaron@greymass.com>
2022-12-16 16:35:43 -0500

    More world formatting

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,19 +1,19 @@
-import { defineMDSveXConfig as defineConfig } from "mdsvex"
+import {defineMDSveXConfig as defineConfig} from 'mdsvex'
 // import codeScreenshot from "remark-code-screenshot"
 // import toc from "remark-toc"
-import rehypeToc from "rehype-toc"
-import rehypeSlug from "rehype-slug"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypeToc from 'rehype-toc'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 const config = defineConfig({
-  extensions: [".svelte.md", ".md", ".svx"],
+    extensions: ['.svelte.md', '.md', '.svx'],
 
-  smartypants: {
-    dashes: "oldschool",
-  },
+    smartypants: {
+        dashes: 'oldschool',
+    },
 
-  remarkPlugins: [],
-  rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeToc],
+    remarkPlugins: [],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeToc],
 })
 
 export default config
