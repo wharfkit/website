825ef481661cf13a5dae0a7018610d2dad1149bc
Dean Sallinen <dean@greymass.com>
2022-12-16 13:53:48 -0800

    fix 500 error on reload

--- src/routes/docs/+layout.ts
+++ src/routes/docs/+layout.ts
@@ -2,7 +2,7 @@ import type {PageLoad} from './$types'
 import {groupBy} from '$lib/utils'
 
 export const load: PageLoad = async ({fetch}) => {
-    const response = await fetch('api/docs')
+    const response = await fetch('/api/docs')
     const docs = await response.json()
 
     const sections = groupBy(docs, 'section')
