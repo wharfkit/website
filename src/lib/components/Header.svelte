6b406e1035901ca13e9b5928fe249115597313e6
Dean Sallinen <dean@greymass.com>
2023-01-05 10:43:25 -0800

    update styles

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,46 +1,72 @@
 <script>
   import logo from "$lib/assets/logos/Wharf-logo-horizontal.svg"
+  import { page } from "$app/stores"
+
+  $: section = $page.url.pathname
+
+  let navItems = [
+    { name: "Blog", href: "/blog" },
+    { name: "Brand", href: "/brand" },
+  ]
 </script>
 
-<header class="switcher">
+<header class="">
   <div class="logo">
     <a href="/">
       <img src={logo} alt="wharf logo" width="203" />
+      <!-- {console.log($page)} -->
     </a>
   </div>
-  <nav class="cluster">
-    <a href="/blog">Blog</a>
-    <a href="/brand">Brand</a>
+  <nav>
+    {#each navItems as { name, href }}
+      <a {href} class:active={new RegExp(href).test(section)}>{name}</a>
+    {/each}
   </nav>
 </header>
 
 <style>
   header {
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
     align-items: center;
     padding-block: var(--s2);
   }
 
   nav {
-    justify-content: flex-end;
-    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--s2);
   }
 
   nav a {
     font-family: var(--ff-heading);
     color: var(--wharf-blue);
     text-decoration: none;
-    padding-inline: 1em;
-    padding-block: 0.5em;
+    /* margin-inline: 1em; */
+    padding-block: var(--s-2);
     border-radius: var(--s-1);
     font-size: var(--fs-0);
-    background-color: none;
-    transition: var(--background-transition);
+    position: relative;
   }
 
-  nav a:is(:hover, :focus-visible) {
-    background-color: var(--swell-mist);
-    /* outline: none; */
+  nav a::after {
+    content: "";
+    background-color: var(--color-secondary-700);
+    opacity: 0;
+    position: absolute;
+    height: 4px;
+    inset: 0;
+    top: auto;
+    border-radius: inherit;
+    transition: opacity 200ms ease-out;
+  }
+
+  nav a:is(:hover, :focus-visible, .active)::after {
+    opacity: 70%;
+  }
+
+  nav a:is(:active)::after {
+    opacity: 100%;
   }
 </style>
