d19ff417390697123cdcab1e16bd09617a548865
Dean Sallinen <dean@greymass.com>
2023-03-28 14:42:01 -0700

    add sort order for documentation sections

--- src/routes/docs/+layout.ts
+++ src/routes/docs/+layout.ts
@@ -2,14 +2,30 @@ import type {LayoutLoad} from './$types'
 import {groupBy} from '$lib/utils'
 import type { DocumentationArticle, DocumentationSections } from '../../lib/types'
 
+
+const sectionOrder = [
+  'Get Started', 
+  'Core Concepts', 
+  'Advanced'
+]
+
 export const load = (async ({fetch}) => {
     const response = await fetch('/api/docs')
     const docs: DocumentationArticle[] = await response.json()
 
     const sections: DocumentationSections = groupBy(docs, 'section')
 
+    // Sort the sections by the order of the sectionOrder array
+    const sortedSections = Object.keys(sections)
+        .sort((a, b) => sectionOrder.indexOf(a) - sectionOrder.indexOf(b))
+        .reduce((acc, key) => {
+            acc[key] = sections[key]
+            return acc
+        }, {} as DocumentationSections)
+
+
     return {
         docs,
-        sections,
+        sections: sortedSections,
     }
 }) satisfies LayoutLoad
