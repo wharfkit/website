7cf4fe80dd0b492599101d974071de9bc0bffa93
Dean Sallinen <dean@greymass.com>
2023-03-30 12:22:01 -0700

    checkbox switcher

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,78 +1,79 @@
 <script>
-  import logo from "$lib/assets/logos/Wharf-logo-horizontal.svg"
-  import ThemeToggle from "./ThemeToggle.svelte"
-  import { page } from "$app/stores"
+    import logo from '$lib/assets/logos/Wharf-logo-horizontal.svg'
+    //   import ThemeToggle from "./ThemeToggle.svelte"
+    import ThemeToggle from './ThemeToggle/index.svelte'
+    import {page} from '$app/stores'
 
-  $: section = $page.url.pathname
+    $: section = $page.url.pathname
 
-  let navItems = [
-    { name: "Blog", href: "/blog" },
-    { name: "Brand", href: "/brand" },
-    { name: "Theme", href: "/theme" },
-  ]
+    let navItems = [
+        {name: 'Blog', href: '/blog'},
+        {name: 'Brand', href: '/brand'},
+        {name: 'Theme', href: '/theme'},
+    ]
 </script>
 
 <header class="">
-  <div class="logo">
-    <a href="/">
-      <img src={logo} alt="wharf logo" width="203" />
-    </a>
-  </div>
-  <nav>
-    {#each navItems as { name, href }}
-      <a {href} class:active={new RegExp(href).test(section)}>{name}</a>
-    {/each}
-    <ThemeToggle />
-  </nav>
+    <div class="logo">
+        <a href="/">
+            <img src={logo} alt="wharf logo" width="203" />
+        </a>
+    </div>
+    <nav>
+        {#each navItems as { name, href }}
+            <a {href} class:active={new RegExp(href).test(section)}>{name}</a>
+        {/each}
+        <ThemeToggle />
+    </nav>
 </header>
 
 <style>
-  header {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    padding-block: var(--s2);
-  }
+    header {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        padding-block: var(--s2);
+    }
 
-  nav {
-    display: flex;
-    flex-wrap: wrap;
-    gap: var(--s2);
-  }
+    nav {
+        display: flex;
+        flex-wrap: wrap;
+        gap: var(--s2);
+    }
 
-  nav a {
-    font-family: var(--ff-heading);
-    color: var(--wharf-blue);
-    color: var(--text1);
-    text-decoration: none;
-    padding-block: var(--s-2);
-    border-radius: var(--s-1);
-    font-size: var(--fs-0);
-    position: relative;
-  }
+    nav a {
+        font-family: var(--ff-heading);
+        color: var(--wharf-blue);
+        color: var(--text1);
+        text-decoration: none;
+        padding-block: var(--s-2);
+        border-radius: var(--s-1);
+        font-size: var(--fs-0);
+        position: relative;
+    }
 
-  nav a::after {
-    content: "";
-    background-color: var(--color-secondary-700);
-    background-color: var(--surface5);
-    opacity: 0;
-    position: absolute;
-    height: 4px;
-    inset: 0;
-    top: auto;
-    border-radius: inherit;
-    transition: opacity 200ms ease-out;
-  }
+    nav a::after {
+        content: '';
+        background-color: var(--color-secondary-700);
+        background-color: var(--surface5);
+        opacity: 0;
+        position: absolute;
+        height: 4px;
+        inset: 0;
+        top: auto;
+        border-radius: inherit;
+        transition: opacity 200ms ease-out;
+    }
 
-  nav a:is(:hover, :focus-visible)::after {
-    opacity: 50%;
-  }
-  nav a:is(.active)::after {
-    opacity: 70%;
-  }
+    nav a:is(:hover, :focus-visible)::after {
+        opacity: 50%;
+    }
+    nav a:is(.active)::after {
+        opacity: 70%;
+    }
 
-  nav a:is(:active)::after {
-    opacity: 100%;
-  }
+    nav a:is(:active)::after {
+        opacity: 100%;
+    }
 </style>
--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -1,35 +1,38 @@
 <script lang="ts">
-  import { onMount } from "svelte"
+    import {onMount} from 'svelte'
 
-  onMount(() => {
-    const switcher = document.querySelector("#theme-switcher")
-    const doc = document.firstElementChild
+    onMount(() => {
+        const switcher = document.querySelector('#theme-switcher')
+        const doc = document.firstElementChild
 
-    switcher.addEventListener("input", (e) => setTheme(e.target.value))
+        switcher.addEventListener('input', (e) => setTheme(e.target.value))
 
-    const setTheme = (theme) => doc.setAttribute("color-scheme", theme)
-  })
+        const setTheme = (theme) => {
+            console.log(theme)
+            doc.setAttribute('color-scheme', theme)
+        }
+    })
 </script>
 
 <form id="theme-switcher">
-  <div>
-    <input checked type="radio" id="auto" name="theme" value="auto" />
-    <label for="auto">Auto</label>
-  </div>
-  <div>
-    <input type="radio" id="light" name="theme" value="light" />
-    <label for="light">Light</label>
-  </div>
-  <div>
-    <input type="radio" id="dark" name="theme" value="dark" />
-    <label for="dark">Dark</label>
-  </div>
+    <div>
+        <input checked type="radio" id="auto" name="theme" value="auto" />
+        <label for="auto">Auto</label>
+    </div>
+    <div>
+        <input type="radio" id="light" name="theme" value="light" />
+        <label for="light">Light</label>
+    </div>
+    <div>
+        <input type="radio" id="dark" name="theme" value="dark" />
+        <label for="dark">Dark</label>
+    </div>
 </form>
 
 <style>
-  form {
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-  }
+    form {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+    }
 </style>
