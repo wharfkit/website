12603486b4be19eb4615ba101da5ffcb5346f41e
Dean Sallinen <dean@greymass.com>
2023-03-30 12:49:05 -0700

    formatting

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,171 +1,170 @@
 <script lang="ts">
-    import logo from '$lib/assets/logos/Wharf-logo-horizontal.svg'
-    import {page} from '$app/stores'
-    import {fly} from 'svelte/transition'
+  import logo from "$lib/assets/logos/Wharf-logo-horizontal.svg"
+  import { page } from "$app/stores"
+  import { fly } from "svelte/transition"
 
-    $: section = $page.url.pathname
+  $: section = $page.url.pathname
 
-    let navItems = [
-        {name: 'Wharf', href: '#'},
-        {name: 'Kits', href: '#'},
-        {name: 'Learn', href: '#'},
-        {name: 'Documentation', href: '#'},
-        {name: 'Blog', href: '/blog'},
-        {name: 'Brand', href: '/brand'},
-    ]
+  let navItems = [
+    { name: "Wharf", href: "#" },
+    { name: "Kits", href: "#" },
+    { name: "Learn", href: "#" },
+    { name: "Documentation", href: "#" },
+    { name: "Blog", href: "/blog" },
+    { name: "Brand", href: "/brand" },
+  ]
 
-    let isNavOpen = false
+  let isNavOpen = false
 
-    function openNav() {
-        isNavOpen = true
-    }
+  function openNav() {
+    isNavOpen = true
+  }
 
-    function closeNav() {
-        isNavOpen = false
-    }
+  function closeNav() {
+    isNavOpen = false
+  }
 
-    function toggleNav() {
-        isNavOpen = !isNavOpen
-    }
+  function toggleNav() {
+    isNavOpen = !isNavOpen
+  }
 </script>
 
 <header class:navHidden={!isNavOpen}>
-    <div class="left">
-        <a href="/">
-            <img src={logo} alt="wharf logo" width="203" />
-        </a>
-        <button class="navToggle close" on:click={toggleNav}>
-            <span class="visually-hidden">{isNavOpen ? 'Close' : 'Open'}</span>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            >
-                {#if isNavOpen}
-                    <line x1="18" y1="6" x2="6" y2="18" />
-                    <line x1="6" y1="6" x2="18" y2="18" />
-                {:else}
-                    <line x1="4" y1="12" x2="20" y2="12" />
-                    <line x1="4" y1="6" x2="20" y2="6" />
-                    <line x1="4" y1="18" x2="20" y2="18" />
-                {/if}
-            </svg>
-        </button>
-    </div>
+  <div class="left">
+    <a href="/">
+      <img src={logo} alt="wharf logo" width="203" />
+    </a>
+    <button class="navToggle close" on:click={toggleNav}>
+      <span class="visually-hidden">{isNavOpen ? "Close" : "Open"}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        {#if isNavOpen}
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        {:else}
+          <line x1="4" y1="12" x2="20" y2="12" />
+          <line x1="4" y1="6" x2="20" y2="6" />
+          <line x1="4" y1="18" x2="20" y2="18" />
+        {/if}
+      </svg>
+    </button>
+  </div>
 
-    <nav>
-        <ul>
-            {#each navItems as { name, href }}
-                <li>
-                    <a {href} class:active={new RegExp(href).test(section)} on:click={closeNav}>
-                        {name}
-                    </a>
-                </li>
-            {/each}
-        </ul>
-    </nav>
+  <nav>
+    <ul>
+      {#each navItems as { name, href }}
+        <li>
+          <a {href} class:active={new RegExp(href).test(section)} on:click={closeNav}>
+            {name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
 
-    <a class="button" href="https://github.com/wharfkit">Github</a>
+  <a class="button" href="https://github.com/wharfkit">Github</a>
 </header>
 
 <style>
+  header {
+    --header-background-color: var(--wharf-blue);
+    --header-text-color: white;
+    background: var(--header-background-color, transparent);
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    gap: var(--space-l);
+    margin-block: var(--space-m);
+    border-radius: var(--space-m);
+  }
+
+  .navToggle {
+    display: none;
+  }
+
+  nav ul {
+    display: flex;
+    gap: var(--space-l);
+    list-style: none;
+    padding-inline: 0;
+  }
+
+  nav a {
+    font-family: var(--ff-heading);
+    color: var(--header-text-color, var(--wharf-blue));
+    text-decoration: none;
+    padding-block: var(--space-3xs);
+    border-radius: var(--space-xs);
+    font-size: var(--fs-0);
+    position: relative;
+  }
+
+  nav a::after {
+    content: "";
+    background-color: var(--color-secondary-700);
+    opacity: 0;
+    position: absolute;
+    height: 4px;
+    inset: 0;
+    top: auto;
+    border-radius: inherit;
+    transition: opacity 200ms ease-out;
+  }
+
+  nav a:is(:hover, :focus-visible)::after {
+    opacity: 50%;
+  }
+  nav a:is(.active)::after {
+    opacity: 70%;
+  }
+
+  nav a:is(:active)::after {
+    opacity: 100%;
+  }
+
+  a.button {
+    justify-self: end;
+    padding-inline: var(--space-xl);
+    margin: var(--space-s);
+  }
+
+  @media (max-width: 768px) {
     header {
-        --header-background-color: var(--wharf-blue);
-        --header-text-color: white;
-        background: var(--header-background-color, transparent);
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        align-items: center;
-        gap: var(--space-l);
-        margin-block: var(--space-m);
-        border-radius: var(--space-m);
+      --nav-background-color: var(--color-primary-900);
+      position: relative;
+      grid-template-columns: 1fr;
+    }
+
+    header .left {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: var(--space-l);
     }
 
     .navToggle {
-        display: none;
+      display: grid;
+      place-items: center;
+      cursor: pointer;
+      background: var(--nav-background-color, var(--swell-mist));
+      padding: var(--space-s);
+      margin: var(--space-xs);
+      border-radius: 50%;
+      aspect-ratio: 1;
+      position: relative;
+      color: var(--header-text-color, var(--wharf-blue));
+      filter: url("#goo");
     }
 
-    nav ul {
-        display: flex;
-        gap: var(--space-l);
-        list-style: none;
-        padding-inline: 0;
-    }
-
-    nav a {
-        font-family: var(--ff-heading);
-        color: var(--header-text-color, var(--wharf-blue));
-        text-decoration: none;
-        padding-block: var(--space-3xs);
-        border-radius: var(--space-xs);
-        font-size: var(--fs-0);
-        position: relative;
-    }
-
-    nav a::after {
-        content: '';
-        background-color: var(--color-secondary-700);
-        opacity: 0;
-        position: absolute;
-        height: 4px;
-        inset: 0;
-        top: auto;
-        border-radius: inherit;
-        transition: opacity 200ms ease-out;
-    }
-
-    nav a:is(:hover, :focus-visible)::after {
-        opacity: 50%;
-    }
-    nav a:is(.active)::after {
-        opacity: 70%;
-    }
-
-    nav a:is(:active)::after {
-        opacity: 100%;
-    }
-
-    a.button {
-        justify-self: end;
-        padding-inline: var(--space-xl);
-        margin: var(--space-s);
-    }
-
-    @media (max-width: 768px) {
-        header {
-            --nav-background-color: var(--color-primary-900);
-            position: relative;
-            grid-template-columns: 1fr;
-        }
-
-        header .left {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            gap: var(--space-l);
-        }
-
-        .navToggle {
-            display: grid;
-            place-items: center;
-            cursor: pointer;
-            background: var(--nav-background-color, var(--swell-mist));
-            padding: var(--space-s);
-            margin: var(--space-xs);
-            border-radius: 50%;
-            aspect-ratio: 1;
-            position: relative;
-            color: var(--header-text-color, var(--wharf-blue));
-            filter: url('#goo');
-        }
-
-        /* .navToggle::after {
+    /* .navToggle::after {
             content: '';
             position: absolute;
             inset: 0;
@@ -191,49 +190,49 @@
             z-index: -1;
         } */
 
-        /* .navHidden .navToggle::after {
+    /* .navHidden .navToggle::after {
             transform: translateY(0) scale(0);
         }
         .navHidden .navToggle::before {
             transform: scaleY(0);
         } */
 
-        nav {
-            /* display: none; */
-            position: absolute;
-            top: calc(100% + var(--space-2xs));
-            flex-direction: column;
-            justify-content: flex-start;
-            gap: var(--space-m);
-            background: var(--nav-background-color, var(--swell-mist));
-            padding: var(--space-l);
-            border-radius: var(--border-radius, var(--space-s));
-            width: 100%;
-            z-index: 999;
-            transform: scaleY(1) translateY(0);
-            transform-origin: top right;
+    nav {
+      /* display: none; */
+      position: absolute;
+      top: calc(100% + var(--space-2xs));
+      flex-direction: column;
+      justify-content: flex-start;
+      gap: var(--space-m);
+      background: var(--nav-background-color, var(--swell-mist));
+      padding: var(--space-l);
+      border-radius: var(--border-radius, var(--space-s));
+      width: 100%;
+      z-index: 999;
+      transform: scaleY(1) translateY(0);
+      transform-origin: top right;
 
-            transition: transform 200ms;
-            transition-timing-function: cubic-bezier(0.23, 1, 0.32, 1);
-        }
-
-        nav ul {
-            display: flex;
-            flex-direction: column;
-            gap: var(--space-m);
-        }
-
-        .navHidden nav {
-            /* transform: translateY(-200%); */
-            transform: scaleY(0) translateY(0);
-            /* transition-delay: 100ms; */
-        }
-        .navHidden nav a {
-            color: var(--header-background-color, var(--swell-mist));
-        }
-
-        a.button {
-            display: none;
-        }
+      transition: transform 200ms;
+      transition-timing-function: cubic-bezier(0.23, 1, 0.32, 1);
     }
+
+    nav ul {
+      display: flex;
+      flex-direction: column;
+      gap: var(--space-m);
+    }
+
+    .navHidden nav {
+      /* transform: translateY(-200%); */
+      transform: scaleY(0) translateY(0);
+      /* transition-delay: 100ms; */
+    }
+    .navHidden nav a {
+      color: var(--header-background-color, var(--swell-mist));
+    }
+
+    a.button {
+      display: none;
+    }
+  }
 </style>
--- src/routes/blog/posts/[slug]/+page.svelte
+++ src/routes/blog/posts/[slug]/+page.svelte
@@ -1,104 +1,102 @@
 <script>
-    import Video from '$lib/components/Video.svelte'
-    import addCopyButton from '$lib/addCopyButton'
-    import {onMount} from 'svelte'
-    /** @type {import('./$types').PageData} */
-    export let data
+  import Video from "$lib/components/Video.svelte"
+  import addCopyButton from "$lib/addCopyButton"
+  import { onMount } from "svelte"
+  /** @type {import('./$types').PageData} */
+  export let data
 
-    let title = data.post.title
-    let date = data.post.date
-    let videolink = data.post.videolink
-    let transcriptlink = data.post.transcriptlink
-    let image = data.post.image
+  let title = data.post.title
+  let date = data.post.date
+  let videolink = data.post.videolink
+  let transcriptlink = data.post.transcriptlink
+  let image = data.post.image
 
-    onMount(() => {
-        addCopyButton()
-    })
+  onMount(() => {
+    addCopyButton()
+  })
 </script>
 
 <article class="center stack">
-    <a href="/blog" class="back">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg
-        >
-        <span>Back to blog</span></a
-    >
-    <header>
-        <h1>{title}</h1>
-        <p>{date}</p>
-    </header>
+  <a href="/blog" class="back">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg>
+    <span>Back to blog</span></a>
+  <header>
+    <h1>{title}</h1>
+    <p>{date}</p>
+  </header>
 
-    {#if image}
-        <img src={image} alt="blog post header" />
-    {/if}
+  {#if image}
+    <img src={image} alt="blog post header" />
+  {/if}
 
-    {#if videolink}
-        <Video {videolink} {transcriptlink} />
-    {/if}
+  {#if videolink}
+    <Video {videolink} {transcriptlink} />
+  {/if}
 
-    {@html data.post.content}
+  {@html data.post.content}
 </article>
 
 <style>
-    header {
-        font-family: var(--ff-heading);
-        font-weight: 600;
-    }
+  header {
+    font-family: var(--ff-heading);
+    font-weight: 600;
+  }
 
-    h1 {
-        font-size: var(--fs-3);
-    }
+  h1 {
+    font-size: var(--fs-3);
+  }
 
-    .back {
-        display: inline-flex;
-        gap: 0.5em;
-        align-items: center;
-        text-decoration: none;
-    }
+  .back {
+    display: inline-flex;
+    gap: 0.5em;
+    align-items: center;
+    text-decoration: none;
+  }
 
-    .back:hover {
-        text-decoration: underline;
-    }
+  .back:hover {
+    text-decoration: underline;
+  }
 
-    article :global(h2) {
-        font-family: var(--ff-heading);
-        font-size: var(--fs-2);
-        font-weight: 600;
-        margin-block-start: var(--space-m);
-    }
+  article :global(h2) {
+    font-family: var(--ff-heading);
+    font-size: var(--fs-2);
+    font-weight: 600;
+    margin-block-start: var(--space-m);
+  }
 
-    article :global(h3) {
-        font-family: var(--ff-heading);
-        font-size: var(--fs-1);
-        font-weight: 600;
-        margin-block-start: var(--space-s);
-    }
+  article :global(h3) {
+    font-family: var(--ff-heading);
+    font-size: var(--fs-1);
+    font-weight: 600;
+    margin-block-start: var(--space-s);
+  }
 
-    article :global(blockquote) {
-        background-color: var(--color-secondary-50);
-        border-inline-start: 0.25em solid var(--color-secondary-200);
-        padding-block: var(--space-s);
-        padding-inline: var(--space-m);
-    }
+  article :global(blockquote) {
+    background-color: var(--color-secondary-50);
+    border-inline-start: 0.25em solid var(--color-secondary-200);
+    padding-block: var(--space-s);
+    padding-inline: var(--space-m);
+  }
 
-    article :global(li) {
-        line-height: var(--line-height);
-    }
+  article :global(li) {
+    line-height: var(--line-height);
+  }
 
-    article :global(li:not(:first-child)) {
-        /* background-color: red; */
-        margin-block-start: var(--space-xs);
-    }
+  article :global(li:not(:first-child)) {
+    /* background-color: red; */
+    margin-block-start: var(--space-xs);
+  }
 
-    img {
-        border-radius: var(--space-s);
-        margin-block: var(--space-m);
-    }
+  img {
+    border-radius: var(--space-s);
+    margin-block: var(--space-m);
+  }
 </style>
