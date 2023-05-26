869c010e362e3ac2be291aca9d8520b718937ad5
Dean Sallinen <dean@greymass.com>
2023-05-26 15:17:09 -0700

    lazy load embedded videos for improved perf

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Button from "$lib/components/Button.svelte"
+  import IntersectionObserver from "./IntersectionObserver.svelte"
 
   export let videolink: string = ""
   export let transcriptlink: string = ""
@@ -29,30 +30,34 @@
   let thumbnail = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`
 </script>
 
-<div class="media | stack">
-  <div class="frame" style="background-image: url({thumbnail}); background-size: cover;">
-    <iframe
-      class="youtube"
-      src={videoEmbed}
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media;"
-      allowfullscreen />
-    <a class="play | imposter box" href={videolink}>
-      <span class="visually-hidden">Watch on YouTube</span>
-      <svg width="34" height="34">
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M5.92725 3.30884V30.6912L29.1917 17L5.92725 3.30884ZM2.83325 2.76959C2.83325 0.626828 5.18123 -0.701815 7.03745 0.39057L31.2182 14.621C33.0383 15.692 33.0383 18.3079 31.2182 19.379L7.03745 33.6094C5.18123 34.7018 2.83325 33.3732 2.83325 31.2304V2.76959Z"
-          fill="white" />
-      </svg>
-    </a>
+<IntersectionObserver let:intersecting once top={400}>
+  <div class="media | stack">
+    <div class="frame" style="background-image: url({thumbnail}); background-size: cover;">
+      {#if intersecting}
+        <iframe
+          class="youtube"
+          src={videoEmbed}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media;"
+          allowfullscreen />
+      {/if}
+      <a class="play | imposter box" href={videolink}>
+        <span class="visually-hidden">Watch on YouTube</span>
+        <svg width="34" height="34">
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M5.92725 3.30884V30.6912L29.1917 17L5.92725 3.30884ZM2.83325 2.76959C2.83325 0.626828 5.18123 -0.701815 7.03745 0.39057L31.2182 14.621C33.0383 15.692 33.0383 18.3079 31.2182 19.379L7.03745 33.6094C5.18123 34.7018 2.83325 33.3732 2.83325 31.2304V2.76959Z"
+            fill="white" />
+        </svg>
+      </a>
+    </div>
+    {#if transcriptlink}
+      <Button link={transcriptlink}>Read transcript (Google docs)</Button>
+    {/if}
   </div>
-  {#if transcriptlink}
-    <Button link={transcriptlink}>Read transcript (Google docs)</Button>
-  {/if}
-</div>
+</IntersectionObserver>
 
 <style>
   .media {
