7c23d0d2fe49d0e14fbac01217abe254a7d149d1
Dean Sallinen <dean@greymass.com>
2023-05-31 12:21:21 -0700

    add styling to all docs pages

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -5,6 +5,35 @@
   const { sections } = data
 </script>
 
+<svelte:head>
+  <style>
+    body[data-theme="light"] {
+      --footer-background: var(--swell-mist);
+      background: linear-gradient(
+        180deg,
+        #494e62 0%,
+        #7be7ce 4rem,
+        #b2f2e1 5.25rem,
+        #f4faf4 7rem,
+        #ffffff 8.75rem
+      );
+    }
+
+    /* prettier-ignore */
+    body[data-theme="dark"] {
+      --footer-background: #262936;
+      background-image: linear-gradient(
+        180deg,
+        #7BE7CE 0%,
+        #494E62 5.25rem,
+        #262936 7rem,
+        #151720 8.75rem
+
+      );
+    }
+  </style>
+</svelte:head>
+
 <div>
   <Sidebar {sections} />
 
--- src/routes/docs/[section]/[article]/+layout.svelte
+++ src/routes/docs/[section]/[article]/+layout.svelte
@@ -42,33 +42,6 @@
   })
 </script>
 
-<svelte:head>
-  <style>
-    body[data-theme="light"] {
-      background: linear-gradient(
-        180deg,
-        #494e62 0%,
-        #7be7ce 4rem,
-        #b2f2e1 5.25rem,
-        #f4faf4 7rem,
-        #ffffff 8.75rem
-      );
-    }
-
-    /* prettier-ignore */
-    body[data-theme="dark"] {
-      background-image: linear-gradient(
-        180deg,
-        #7BE7CE 0%,
-        #494E62 5.25rem,
-        #262936 7rem,
-        #151720 8.75rem
-
-      );
-    }
-  </style>
-</svelte:head>
-
 <main>
   <article>
     <!-- <Breadcrumbs {breadcrumbs} /> -->
