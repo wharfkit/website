bfe4ba51fdd3adc939ae8e4818db65d3de3492f6
Dean Sallinen <dean@greymass.com>
2023-07-04 13:14:24 -0700

    add github icon to header button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -67,7 +67,23 @@
     </menu>
 
     <div class="right">
-      <a class="button" href="https://github.com/wharfkit">Github</a>
+      <a class="button" href="https://github.com/wharfkit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-github"
+          ><path
+            d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path
+            d="M9 18c-4.51 2-5-2-7-2" /></svg>
+        <span>Github</span>
+      </a>
     </div>
   </nav>
 </header>
@@ -123,7 +139,7 @@
     justify-content: space-between;
     list-style: none;
     padding-inline: 0;
-    /* gap: var(--space-s); */
+    /* gap: var(--space-2xs); */
   }
 
   menu a {
@@ -132,7 +148,6 @@
     text-decoration: none;
     padding-block: var(--space-xs);
     padding-inline: var(--space-xs);
-    border-radius: var(--space-xs);
     border-radius: 12px;
     font-size: var(--fs-0);
   }
@@ -149,11 +164,12 @@
   }
 
   a.button {
-    justify-self: end;
-    padding-inline: var(--space-xl);
+    padding-inline-start: 2.75rem;
+    padding-inline-end: 3rem;
     margin-inline: var(--space-2xs);
     margin-block: var(--space-2xs);
     font-family: var(--ff-body);
+    font-size: var(--fs-0);
   }
 
   @media (max-width: 768px) {
