3a4f3a752e253cfe8f2c0bd88d01545cc949b7e2
Dean Sallinen <dean@greymass.com>
2023-07-18 15:08:38 -0700

    fix typo

--- src/routes/brand/+page.svelte
+++ src/routes/brand/+page.svelte
@@ -33,10 +33,10 @@
         <h1>Brand Guidelines</h1>
         <p>
           Thank you for your interest in the Wharf brand! We would like to make it as easy as
-          possible for you to reference Wharf in your project, so we've prepared all the
-          essentials for you here: the logo, colors, font, and background images. If you would like
-          to go further in your usage of the Wharf brand, please download the Brand Guidelines
-          PDF for additional images and information.
+          possible for you to reference Wharf in your project, so we've prepared all the essentials
+          for you here: the logo, colors, font, and background images. If you would like to go
+          further in your usage of the Wharf brand, please download the Brand Guidelines PDF for
+          additional images and information.
         </p>
         <div class="buttons | cluster">
           <a class="button" href="https://assets.wharfkit.com/wharf-brand-assets.zip" download
@@ -45,7 +45,7 @@
             class="button"
             data-type="secondary"
             href="https://assets.wharfkit.com/wharf-brand-guidelines.pdf"
-            download>Download Brand Buidelines (PDF)</a>
+            download>Download Brand Guidelines (PDF)</a>
         </div>
       </section>
       <hr />
