1caaf48a85953b2094404e8a633a6826e2fae96e
Dean Sallinen <dean@greymass.com>
2023-08-09 12:43:20 -0700

    fix typo

--- src/routes/kits/[kit]/+page.svelte
+++ src/routes/kits/[kit]/+page.svelte
@@ -156,7 +156,7 @@
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
-              viewbox="0 0 24 24"
+              viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
               stroke-width="2"
