8a1465ca25987b2b060711a8d26fad6f80eed544
Dean Sallinen <dean@greymass.com>
2023-08-15 15:32:58 -0700

    add buttons to guides section

--- src/routes/guides/[section]/[slug]/+page.svelte
+++ src/routes/guides/[section]/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import type { PageData } from "./$types"
+  import PageButtons from "$lib/components/PageButtons.svelte"
   export let data: PageData
 </script>
 
 <article id="article">
   {@html data.doc.content}
 </article>
+
+<PageButtons currentDoc={data.doc} allSections={data.docs} />
