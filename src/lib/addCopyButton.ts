fc97441d5ac439ddcb525f2cb03d1351b300c666
Dean Sallinen <dean@greymass.com>
2023-08-17 10:55:53 -0700

    add copy to clipboard button to docs and guides (#132)

--- src/lib/addCopyButton.ts
+++ src/lib/addCopyButton.ts
@@ -1,5 +1,5 @@
-const icon = `<svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>`
-const iconSuccess = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="8" y="2" width="8" height="4" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="m9 14 2 2 4-4"/></svg>`
+const icon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>`
+const iconSuccess = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="8" y="2" width="8" height="4" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="m9 14 2 2 4-4"/></svg>`
 
 export default function copyButton(
   selector = "pre",
@@ -16,7 +16,7 @@ export default function copyButton(
     node.appendChild(copyBtn)
     copyBtn.addEventListener("click", async () => {
       if (navigator.clipboard) {
-        let text = node.querySelector(childSelector).innerText
+        let text = node.querySelector(childSelector)?.innerText
         await navigator.clipboard.writeText(text)
       }
       copyBtn.innerHTML = btnTextSuccess
