6d4c287145bfad48aea8706b1a888f34b084cdbb
Dean Sallinen <dean@greymass.com>
2023-08-18 12:36:52 -0700

    display logic for scroll top button

--- src/lib/components/ScrollToTop.svelte
+++ src/lib/components/ScrollToTop.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
   import { scrollToTop } from "$lib/utils"
+  import { scrollDirection, scrollPosition } from "$lib/stores"
+  import { fade } from "svelte/transition"
+
+  let innerHeight: number
 </script>
 
-<button on:click={scrollToTop}>
-  <span class="visually-hidden">Scroll to top</span>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="lucide lucide-arrow-up"><path d="m5 12 7-7 7 7" /><path d="M12 19V5" /></svg>
-</button>
+<svelte:window bind:innerHeight />
+
+{#if $scrollPosition > innerHeight * 1.5}
+  <button
+    on:click={scrollToTop}
+    data-direction={$scrollDirection}
+    transition:fade={{ duration: 300 }}>
+    <span class="visually-hidden">Scroll to top</span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-arrow-up"><path d="m5 12 7-7 7 7" /><path d="M12 19V5" /></svg>
+  </button>
+{/if}
 
 <style>
   :global([data-theme="light"]) {
