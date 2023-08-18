b9462a6f50f2c7556866b72a7c2553af090b1c47
Dean Sallinen <dean@greymass.com>
2023-08-18 16:32:53 -0700

    Blog design update (#141)
    * update blog design
    
    * fix header margin

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -57,12 +57,10 @@
 
 <main>
   <article class="stack">
-    {#if image}
-      <img src={image} alt="blog post header" />
-    {/if}
-
     {#if videolink}
       <Video {videolink} {transcriptlink} />
+    {:else if image}
+      <img src={image} alt="blog post header" />
     {/if}
 
     <header>
