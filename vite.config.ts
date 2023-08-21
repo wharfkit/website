e42b681f404258a984e53f7db9adaecfb343d87c
Dean Sallinen <dean@greymass.com>
2023-08-21 11:36:06 -0700

    add support for ssl in dev server (#134)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite"
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import type { UserConfig } from "vite"
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [basicSsl(), sveltekit()],
   server: {
     host: true,
+    https: true,
   },
   css: {
     preprocessorOptions: {
