50cb0a06d72062ed561fbe3060d375c0d7660b15
Aaron Cox <aaron@greymass.com>
2023-08-23 17:10:51 -0700

    Added ContractKit to Guides

--- src/routes/guides/+layout.server.ts
+++ src/routes/guides/+layout.server.ts
@@ -1,22 +1,20 @@
-import type { LayoutServerLoad } from './$types'
-import { fetchGroupedDocs, orderSections, importedGuides } from "$lib/utils";
+import type { LayoutServerLoad } from "./$types"
+import { fetchGroupedDocs, orderSections, importedGuides } from "$lib/utils"
 
 /**
  * Order the sections here.
  */
-const displayOrder = [
-    'SessionKit',
-];
+const displayOrder = ["SessionKit", "ContractKit"]
 
 export const prerender = true
 
 export const load = (async () => {
-    const groupedDocs = await fetchGroupedDocs(importedGuides)
-    const orderedDocs = orderSections(groupedDocs, displayOrder)
+  const groupedDocs = await fetchGroupedDocs(importedGuides)
+  const orderedDocs = orderSections(groupedDocs, displayOrder)
 
-    return {
-        rootPath: '/guides',
-        rootTitle: 'Guides',
-        docs: orderedDocs,
-    }
+  return {
+    rootPath: "/guides",
+    rootTitle: "Guides",
+    docs: orderedDocs,
+  }
 }) satisfies LayoutServerLoad
