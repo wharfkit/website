e510884aea711e0f73ae9647371148e408a4bb04
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2023-12-20 06:04:42 -0800

    feat: add workflow to check plugins

---
+++ .github/scripts/check-plugins.js
@@ -0,0 +1,73 @@
+import { readFile } from "fs/promises"
+import https from "https"
+
+const FILE_PATH = "./src/lib/plugin-directory.txt"
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+/**
+ * @param {string} filePath
+ */
+async function importTxtFile(filePath) {
+  try {
+    const text = await readFile(filePath, "utf-8")
+    const lines = text.split("\n").map((line) => line.trim())
+
+    return lines
+  } catch (error) {
+    console.error(`Error importing file: ${error.message}`)
+    return []
+  }
+}
+
+/**
+ * @param {string} repo
+ * */
+async function fetchRepoData(repo) {
+  const url = `https://api.github.com/repos/${repo}/commits/main`
+  const options = {
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      "User-Agent": "GitHub Actions",
+    },
+  }
+
+  let json = ""
+
+  https
+    .get(url, options, (res) => {
+      let data = ""
+
+      res.on("data", (chunk) => {
+        data += chunk
+      })
+
+      res.on("end", () => {
+        if (res.statusCode === 200) {
+          json = JSON.parse(data)
+        } else {
+          console.error(
+            `Error getting commit information for ${repo}. Status code: ${res.statusCode}`
+          )
+        }
+      })
+    })
+    .on("error", (error) => {
+      console.error(`Error making API request: ${error.message}`)
+    })
+
+  return json
+}
+
+async function main() {
+  try {
+    const repositories = await importTxtFile(FILE_PATH)
+    repositories.forEach(async (repo) => {
+      const json = await fetchRepoData(repo)
+      console.log(json)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
